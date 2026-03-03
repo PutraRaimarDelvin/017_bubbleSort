7ba90cf1aa8048f804c396c725f2d842aa994660
PutraRaimarDelvin <delvinputra805@gmail.com>
2026-03-03 11:03:09 +0700

    Menambahkan fungsi input() dengan validasi maksimal 20 elemen

--- bubble.short.cpp
+++ bubble.short.cpp
@@ -4,6 +4,24 @@ using namespace std;
 int a[20];
 int n;
 
+void input() {
+    while (true) {
+        cout << "Masukkan banyaknya elemen pada array : ";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "Array maksimal 20 elemen.\n";
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> a[i];
+    }
+}
+
 int main() {
+    input();
     return 0;
 }
